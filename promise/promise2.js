20660a818bb8927503b6cd6bacbbce7fd91bc4f1
wv1344 <wv1344@gmail.com>
2021-08-19 12:00:05 +0800

    study

--- promise/promise2.js
+++ promise/promise2.js
@@ -17,12 +17,12 @@ class MyPromise {
   // 存储状态的变量，初始值 pending
   status = PENDING;
   // 成功之后的值
-  value = null;
+  value = null
   // 失败之后的值
   reason = null
 
   // 存储成功回调函数
-  onFulfilledCallbacks = [];
+  onFulfilledCallbacks = []
   // 存储失败回调函数
   onRejectedCallbacks = []
 
@@ -30,9 +30,9 @@ class MyPromise {
     // 只有等待的状态，才可以执行状态修改
     if (this.status === PENDING) {
       // 状态修改为成功
-      this.status = FULFILLED;
+      this.status = FULFILLED
       // 保存成功之后的值
-      this.value = value;
+      this.value = value
 
       // 判断回调函数是否存在，存在就调用
       while (this.onFulfilledCallbacks.length) {
@@ -45,9 +45,9 @@ class MyPromise {
     // 只有等待的状态，才可以执行状态修改
     if (this.status === PENDING) {
       // 状态修改为失败
-      this.status = REJECTED;
+      this.status = REJECTED
       // 保存失败的原因
-      this.reason = reason;
+      this.reason = reason
 
       // 判断回调函数是否存在，存在就调用
       while (this.onRejectedCallbacks.length) {
@@ -62,24 +62,24 @@ class MyPromise {
       if (this.status === FULFILLED) {
         // FULFILLED
         // queueMicrotask(() => {  // 创建一个微任务 等待 promise2 完成初始化
-          try {
-            // 获取成功回调函数的执行结果
-            const x = onFulfilled(this.value);
-            // 传入 resolvePromise 集中处理 返回值 x
-            resolvePromise(x, resolve, reject);
-          } catch (e) {
-            reject(e);
-          }
+        try {
+          // 获取成功回调函数的执行结果
+          const x = onFulfilled(this.value);
+          // 传入 resolvePromise 集中处理 返回值 x
+          resolvePromise(x, resolve, reject);
+        } catch (e) {
+          reject(e);
+        }
         // })
       } else if (this.status === REJECTED) {
         // REJECTED
         // queueMicrotask(() => {
-          try {
-            const x = onRejected(this.reason)
-            resolvePromise(x, resolve, reject)
-          } catch (e) {
-            reject(e)
-          }
+        try {
+          const x = onRejected(this.reason)
+          resolvePromise(x, resolve, reject)
+        } catch (e) {
+          reject(e)
+        }
         // })
       } else if (this.status === PENDING) {
         // PENDING
@@ -101,16 +101,6 @@ function resolvePromise(x, resolve, reject) {
   } else {
     resolve(x)
   }
-}
-
-
-MyPromise.deferred = function () {
-  var result = {};
-  result.promise = new MyPromise(function (resolve, reject) {
-    result.resolve = resolve
-    result.reject = reject
-  })
-  return result
 }
 
 module.exports = MyPromise
